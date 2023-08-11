71c31c15053293c6fd2a842061b1efd1fc1d7165
Lance Armah-Abraham <lance@vdc.cloud>
2023-08-11 19:01:08 +0000

    feat: add about page

---
+++ frontend/src/app/(marketing)/about/page.tsx
@@ -0,0 +1,32 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us - EMS",
+};
+
+export default async function Page() {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-center text-5xl font-bold">About Us</h1>
+
+      <p>
+        Our event management system website is dedicated to providing seamless
+        and efficient solutions for planning and organizing events.
+      </p>
+      <p>
+        With a user-friendly interface and powerful features, we aim to simplify
+        the event management process. From registration to ticketing, our
+        platform offers a comprehensive suite of tools to ensure a successful
+        event.
+      </p>
+
+      <p>
+        With our experience and expertise, we strive to deliver exceptional
+        service and exceed our clients' expectations. Whether it's a corporate
+        conference or a social gathering, our event management system is
+        designed to streamline the entire event planning process and create
+        unforgettable experiences.
+      </p>
+    </div>
+  );
+}
