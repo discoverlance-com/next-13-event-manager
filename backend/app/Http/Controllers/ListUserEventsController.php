96a296f54440b1a869d495e66b35393237bce8f3
Lance Armah-Abraham <lance@vdc.cloud>
2023-09-06 19:19:59 +0000

    stop user events caching because of pagination

--- backend/app/Http/Controllers/ListUserEventsController.php
+++ backend/app/Http/Controllers/ListUserEventsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class ListUserEventsController extends Controller
 {
@@ -16,10 +15,8 @@ class ListUserEventsController extends Controller
     {
         //
         $this->authorize('viewAny', Event::class);
-        return Cache::remember($request->user()->id . '_events', 60 * 60 * 1, function () use ($request) {
-            return EventResource::collection(Event::where('user_id', $request->user()->id)->paginate())
+        return EventResource::collection(Event::where('user_id', $request->user()->id)->paginate())
                 ->response()
                 ->setStatusCode(200);
-        });
     }
 }
